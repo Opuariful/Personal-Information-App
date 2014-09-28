b3609cf9ae2d49031e11818b8f65c80977df273e
Opuariful <oopu.ariful@gmail.com>
2014-09-28 20:49:43 +0600

    Personal Information Application.

---
+++ Personal Information/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Personal_Information
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showAllInformationButton = new System.Windows.Forms.Button();
+            this.nameButton = new System.Windows.Forms.Button();
+            this.parentsNameButton = new System.Windows.Forms.Button();
+            this.addressButton = new System.Windows.Forms.Button();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.fathersNameTextBox = new System.Windows.Forms.TextBox();
+            this.mothersNameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(113, 332);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showAllInformationButton
+            // 
+            this.showAllInformationButton.Location = new System.Drawing.Point(238, 332);
+            this.showAllInformationButton.Name = "showAllInformationButton";
+            this.showAllInformationButton.Size = new System.Drawing.Size(118, 23);
+            this.showAllInformationButton.TabIndex = 1;
+            this.showAllInformationButton.Text = "Show All Information";
+            this.showAllInformationButton.UseVisualStyleBackColor = true;
+            this.showAllInformationButton.Click += new System.EventHandler(this.showAllInformationButton_Click);
+            // 
+            // nameButton
+            // 
+            this.nameButton.Location = new System.Drawing.Point(80, 371);
+            this.nameButton.Name = "nameButton";
+            this.nameButton.Size = new System.Drawing.Size(75, 23);
+            this.nameButton.TabIndex = 2;
+            this.nameButton.Text = "Name";
+            this.nameButton.UseVisualStyleBackColor = true;
+            this.nameButton.Click += new System.EventHandler(this.nameButton_Click);
+            // 
+            // parentsNameButton
+            // 
+            this.parentsNameButton.Location = new System.Drawing.Point(180, 371);
+            this.parentsNameButton.Name = "parentsNameButton";
+            this.parentsNameButton.Size = new System.Drawing.Size(106, 23);
+            this.parentsNameButton.TabIndex = 3;
+            this.parentsNameButton.Text = "Parents Name";
+            this.parentsNameButton.UseVisualStyleBackColor = true;
+            this.parentsNameButton.Click += new System.EventHandler(this.parentsNameButton_Click);
+            // 
+            // addressButton
+            // 
+            this.addressButton.Location = new System.Drawing.Point(301, 371);
+            this.addressButton.Name = "addressButton";
+            this.addressButton.Size = new System.Drawing.Size(75, 23);
+            this.addressButton.TabIndex = 4;
+            this.addressButton.Text = "Address";
+            this.addressButton.UseVisualStyleBackColor = true;
+            this.addressButton.Click += new System.EventHandler(this.addressButton_Click);
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(190, 58);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(227, 20);
+            this.firstNameTextBox.TabIndex = 5;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(190, 103);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(227, 20);
+            this.lastNameTextBox.TabIndex = 6;
+            // 
+            // fathersNameTextBox
+            // 
+            this.fathersNameTextBox.Location = new System.Drawing.Point(190, 147);
+            this.fathersNameTextBox.Name = "fathersNameTextBox";
+            this.fathersNameTextBox.Size = new System.Drawing.Size(227, 20);
+            this.fathersNameTextBox.TabIndex = 7;
+            // 
+            // mothersNameTextBox
+            // 
+            this.mothersNameTextBox.Location = new System.Drawing.Point(190, 191);
+            this.mothersNameTextBox.Name = "mothersNameTextBox";
+            this.mothersNameTextBox.Size = new System.Drawing.Size(227, 20);
+            this.mothersNameTextBox.TabIndex = 8;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(190, 240);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(227, 20);
+            this.addressTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Father\'s Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Mother\'s Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(190, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Personal Information";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 406);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.mothersNameTextBox);
+            this.Controls.Add(this.fathersNameTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.addressButton);
+            this.Controls.Add(this.parentsNameButton);
+            this.Controls.Add(this.nameButton);
+            this.Controls.Add(this.showAllInformationButton);
+            this.Controls.Add(this.saveButton);
+            this.Name = "Form1";
+            this.Text = "Personal Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showAllInformationButton;
+        private System.Windows.Forms.Button nameButton;
+        private System.Windows.Forms.Button parentsNameButton;
+        private System.Windows.Forms.Button addressButton;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox fathersNameTextBox;
+        private System.Windows.Forms.TextBox mothersNameTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Personal Information/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personal_Information
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string firstName = "";
+        string lastName = "";
+        string fathersName = "";
+        string mothersName = "";
+        string address = "";
+
+        
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            firstName = firstNameTextBox.Text;
+            lastName = lastNameTextBox.Text;
+            fathersName = fathersNameTextBox.Text;
+            mothersName = mothersNameTextBox.Text;
+            address = addressTextBox.Text;
+
+
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            fathersNameTextBox.Text = "";
+            mothersNameTextBox.Text = "";
+            addressTextBox.Text = "";
+
+
+        }
+
+        private void nameButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("FirstName:" + firstName + "\n LastName:" + lastName);
+        }
+
+        private void showAllInformationButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("FirstName:" + firstName + "\n LastName:" + lastName + "\n father's Name :" + fathersName + "\n Mother's Name :" + mothersName + "\n Address :" + address);
+        }
+
+        private void parentsNameButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("father's Name :" + fathersName + "\n Mother's Name :" + mothersName + "\n Address :" + address);
+        }
+
+        private void addressButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Address :" + address);
+        }
+    }
+}
